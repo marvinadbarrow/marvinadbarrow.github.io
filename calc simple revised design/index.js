f7226565cbc019fdf036149417622ee1cbcbc7ee
Marvin <marvinbarrow@gmail.com>
2022-05-02 23:58:32 +0100

    second display added

--- calc simple revised design/index.js
+++ calc simple revised design/index.js
@@ -5,7 +5,7 @@ let sumEl = document.getElementById("sum-el")
 let num1El = document.getElementById("a-el")
 let num2El = document.getElementById("b-el")
 document.getElementById("display-el").value = 0;
-
+document.getElementById("displayab-el").value = 0;
 
 function genNum(){
    num1 = Math.floor((Math.random()*10 ))
@@ -13,7 +13,7 @@ function genNum(){
 
    if (num1 === 0){ return 1}
    if (num2 === 0){ return 1}
-  document.getElementById("display-el").value = num1 + "|" + num2;
+  document.getElementById("displayab-el").value = num1 + "|" + num2;
 
 }
 
