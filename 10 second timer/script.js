bd834b232ac014e11ad423dba614fbfae585026f
Marvin <marvinbarrow@gmail.com>
2022-05-27 11:53:39 +0100

    New Game, new timer, non-repeating cards code for blackjack

---
+++ 10 second timer/script.js
@@ -0,0 +1,115 @@
+
+
+let audio = new Audio("cardflip.mp3");
+let timerEl = document.getElementById('counter')
+
+
+
+
+
+
+
+
+function startTimer(){
+audio.play()
+if (timerEl.textContent = 0){ 
+startSlowWrite();
+}}
+
+
+
+
+
+function startSlowWrite(){
+function slowWrite0(){
+
+
+timerEl.textContent = 0
+}
+setTimeout(function(){slowWrite0()},1000);
+
+function slowWrite1(){
+timerEl.textContent = 1
+}
+setTimeout(function(){slowWrite1()},1000);
+
+function slowWrite2(){
+timerEl.textContent = 2
+}
+setTimeout(function(){slowWrite2()},2000);
+
+function slowWrite3(){
+timerEl.textContent = 3
+}
+setTimeout(function(){slowWrite3()},3000);
+
+function slowWrite4(){
+timerEl.textContent = 4
+}
+setTimeout(function(){slowWrite4()},4000);
+
+function slowWrite5(){
+timerEl.textContent = 5
+}
+setTimeout(function(){slowWrite5()},5000);
+
+function slowWrite6(){
+timerEl.textContent = 6
+}
+setTimeout(function(){slowWrite6()},6000);
+
+function slowWrite7(){
+timerEl.textContent = 7
+}
+setTimeout(function(){slowWrite7()},7000);
+
+function slowWrite8(){
+timerEl.textContent = 8
+}
+setTimeout(function(){slowWrite8()},8000);
+
+function slowWrite9(){
+timerEl.textContent = 9
+}
+setTimeout(function(){slowWrite9()},9000);
+
+function slowWrite10(){
+timerEl.textContent = 10
+}
+setTimeout(function(){slowWrite10()},10000);
+
+function slowWrite0(){
+timerEl.textContent = 0
+}
+setTimeout(function(){slowWrite0()},11000);
+
+
+playAudio()
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
