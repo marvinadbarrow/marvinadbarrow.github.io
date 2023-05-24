12b4d4e1b6485af68fe253015bfa6c53844c55bd
Marvin <bugsbunny@gmail.com>
2023-05-24 20:30:42 +0100

    App ages added
    App ages function written to dynamically load app age using 'app created date' with Date object.

--- Game Apps - ALL github/script.js
+++ Game Apps - ALL github/script.js
@@ -10,20 +10,70 @@ let tetrisDeskAge = document.getElementById('tetris_desktop_age');
 let tetrisMobAge = document.getElementById('tetris_mobile_age');
 let snakeDeskAge = document.getElementById('snake_desktop_age')
 
-// get initial date of each game and assign a variable
-let blackjackCreated = new Date('2022/04/30');
-let blackjackRevampedCreated = new Date('2022/05/10');
-let blackjackUpdated = new Date('2023/02/17');
-let memoryCreated = new Date('2022/05/26');
-let solitaireCreated = new Date('2022/11/30');
-let puzzleCreated = new Date(''); 
-let puzzleUpdated = new Date('');
-let tetrisDeskCreated = new Date('');
-let tetrisMobCreated = new Date('');
-let snakeCreated = new Date(''); 
 
-console.log(blackjackCreated, blackjackRevampedCreated)
+// object contains game age and element for rendering age
+let gamesCreatedObj = {
+    blackjackCreated :['2022/04/30', blackJackOldAge],
+    blackjackRevampedCreated :['2022/05/10', blackjacRevampAge],
+    blackjackUpdated :['2023/02/17', blackJackUpdatedAge],
+    memoryCreated :['2022/05/26', memoryAge],
+    solitaireCreated :['2022/11/30', solitaireAge],
+    puzzleCreated :['2022/12/11', puzzleOriginalAge], 
+    puzzleUpdated :['2023/02/24', puzzleAge],
+    tetrisDeskCreated :['2023/01/31', tetrisDeskAge],
+    tetrisMobCreated :['2023/02/01', tetrisMobAge],
+    snakeCreated :['2023/02/07', snakeDeskAge], 
+}
 
+
+let gameAgeArr = Object.keys(gamesCreatedObj)
+
+// current date variable, and date broken down into different time measures, days, months, hours etc. 
+let date = new Date()
+let year = date.getFullYear();
+let month = date.getUTCMonth() + 1;
+let day = date.getUTCDate()
+let hour = (date.getUTCHours()+1)%24; 
+let mins = date.getUTCMinutes();
+
+// function for rendering game age to 'age' element in stats section of each thumbnail container
+const gameAge = (gameDates) =>{
+ gameDates.forEach(game =>{
+    console.log(game, gamesCreatedObj[`${game}`])
+ 
+
+// published date variable below takes the created date for each game and then extracts time measures from year down to minutes
+
+let published = new Date(gamesCreatedObj[`${game}`][0]); // date format
+let uploadYear =  published.getFullYear();
+let uploadMonth =  published.getMonth();
+let uploadDayDate =  published.getDate();
+let uploadHour = published.getHours();
+let uploadMins = published.getMinutes();
+
+// variables for calculating the difference between current date and game created date, which is the age of the game (rendered in the appropriate time measure; years, day, hour etc)
+let yearCalc = year - uploadYear;
+let monthCalc = month - uploadMonth;
+let dayCalc = day - uploadDayDate;
+let hourCalc = hour - uploadHour;
+let minsCalc = uploadMins;
+let age; // age value will be assigned this variable via a switch statement.
+
+
+// code for rending age, NEEDS TO BE INSIDE FOREACH to inspect each video
+if(yearCalc > 0){if(yearCalc > 1){age = `${yearCalc} years ago`}else{age = `${yearCalc} year ago`}}
+else if(monthCalc > 0){if(monthCalc > 1){age = `${monthCalc} months ago`}else{age = `${monthCalc} month ago`}}
+else if(dayCalc > 0){if(dayCalc > 1){age = `${dayCalc} days ago`}else{age = `${dayCalc} day ago`}}
+else if(hourCalc > 0){if(hourCalc > 1){age = `${hourCalc} hours ago`}else{age = `${hourCalc} hour ago`}}
+else if(minsCalc > 0){if(minsCalc > 1){age = `${minsCalc} mins ago`}else{age = `${minsCalc} mins ago`}}
+
+console.log(age)
+gamesCreatedObj[`${game}`][1].textContent = age;
+
+
+});
+}
+gameAge(gameAgeArr)
 
 
 let blackjackOriginalEl = document.getElementById('game-1-thumbnail')
@@ -81,3 +131,5 @@ gameElementsArr.forEach(element =>{
 
 })
 
+
+ 
