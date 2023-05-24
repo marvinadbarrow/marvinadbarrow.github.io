63606492dd188fd944192701995008a0229dc148
Marvin <bugsbunny@gmail.com>
2023-05-24 17:23:38 +0100

    adding first version of puzzle
    Adding the first version of 15 piece puzzle to the games main page, along old thumbnail PNG and GIF animation.

--- Game Apps - ALL github/script.js
+++ Game Apps - ALL github/script.js
@@ -4,6 +4,7 @@ let blackjacRevampAge = document.getElementById('blackJack_revamp_age')
 let blackJackUpdatedAge = document.getElementById('blackjack_new_age');
 let memoryAge = document.getElementById('memory_age');
 let solitaireAge = document.getElementById('solitaire_age');
+let puzzleOriginalAge = document.getElementById('original_puzzle_age');
 let puzzleAge = document.getElementById('15_piece_puzzle_age');
 let tetrisDeskAge = document.getElementById('tetris_desktop_age');
 let tetrisMobAge = document.getElementById('tetris_mobile_age');
@@ -12,13 +13,14 @@ let snakeDeskAge = document.getElementById('snake_desktop_age')
 // get initial date of each game and assign a variable
 let blackjackCreated = new Date('2022/04/30');
 let blackjackRevampedCreated = new Date('2022/05/10');
-let blackjackUpdated = new Date();
-let memoryCreated = new Date();
-let solitaireCreated = new Date();
-let puzzleCreated = new Date();
-let tetrisDeskCreated = new Date();
-let tetrisMobCreated = new Date();
-let snakeCreated = new Date(); 
+let blackjackUpdated = new Date('2023/02/17');
+let memoryCreated = new Date('2022/05/26');
+let solitaireCreated = new Date('2022/11/30');
+let puzzleCreated = new Date(''); 
+let puzzleUpdated = new Date('');
+let tetrisDeskCreated = new Date('');
+let tetrisMobCreated = new Date('');
+let snakeCreated = new Date(''); 
 
 console.log(blackjackCreated, blackjackRevampedCreated)
 
@@ -29,12 +31,13 @@ let blackjackRevampEl = document.getElementById('game-1b-thumbnail')
 let blackjackNewEl = document.getElementById('game-2-thumbnail')
 let memoryEl = document.getElementById('game-3-thumbnail')
 let solitaireEl = document.getElementById('game-4-thumbnail')
+let puzzleOriginalEl = document.getElementById('game-5b-thumbnail')
 let fifteenPuzzleEl = document.getElementById('game-5-thumbnail')
 let tetrisDesktopEl = document.getElementById('game-6-thumbnail')
 let tetrisMobileEl = document.getElementById('game-7-thumbnail')
 let snakeEl = document.getElementById('game-8-thumbnail')
 
-let gameElementsArr = [blackjackOriginalEl, blackjackRevampEl, blackjackNewEl, memoryEl, solitaireEl, fifteenPuzzleEl, tetrisDesktopEl, tetrisMobileEl, snakeEl]
+let gameElementsArr = [blackjackOriginalEl, blackjackRevampEl, blackjackNewEl, memoryEl, solitaireEl, puzzleOriginalEl, fifteenPuzzleEl, tetrisDesktopEl, tetrisMobileEl, snakeEl]
 
 let gifAddressArr = [
     './blackjack original.gif',
@@ -42,6 +45,7 @@ let gifAddressArr = [
     './blackjack new.gif',
     './memory.gif',
     './solitaire.gif',
+    './puzzle original.gif',
     './puzzle.gif',
     './tetris desktop.gif',
     './tetris mobile.gif',
@@ -55,6 +59,7 @@ let imageAddressArr = [
     './games thumbnails/thumbnail blackjack.png',
     './games thumbnails/thumbnail memory.png',
     './games thumbnails/thumbnail solitaire.png',
+    './games thumbnails/thumbnail 15 piece puzzle original.png',
     './games thumbnails/thumbnail 15 piece puzzle upgrade.png',
     './games thumbnails/thumbnail tetris desktop.png',
     './games thumbnails/thumbnail tetris mobile.png',
